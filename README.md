ebbf02d7178e33a9b0af821461a15b7942dfa976
Frankie Hui <frankie@cloudflare.com>
2020-12-04 14:18:42 +0800

    inital commit

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # routeview_loop
 Script to check if certain prefix exists in the Internet
+
+# Install
+```
+pip install -r requirements.txt
+```
+# RUN
+```
+○ → python routeview.py  -h
+usage: routeview.py [-h] -r PREFIX -a ASN -s SLEEP -t TRANSIT
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -r PREFIX, --prefix PREFIX
+                        Check ipv4 route in routing table
+  -a ASN, --asn ASN     ASN that we want to check
+  -s SLEEP, --sleep SLEEP
+                        Sleep for how long
+  -t TRANSIT, --transit TRANSIT
+                        A list of transit asns eg:
+                        '174,1299,3356,5511,3257,2914,7922,1221,4637'
+
+○ → python routeview.py  -r 1.1.1.0/24 -a 13335 -s 30 -t '174,1299,3356,5511,3257,2914,7922,1221,4637'
+[14:16:16] connecting to routeview                                                                                                                                                                    routeview.py:64
+[14:16:17] sending username                                                                                                                                                                           routeview.py:67
+           connected to routeview                                                                                                                                                                     routeview.py:70
+[14:16:19] transit path:9 unique transit:{'1221', '2914', '3257', '3356'}                                                                                                                             routeview.py:57
+           peering path:19                                                                                                                                                                            routeview.py:60
+
+```
---
+++ routeview.py
@@ -0,0 +1,81 @@
+#!/bin/env python
+
+import argparse
+from rich.console import Console
+import pexpect
+import time
+import sys
+
+console = Console()
+try:
+    ArgParse = argparse.ArgumentParser()
+    ArgParse.add_argument(
+        "-r",
+        "--prefix",
+        type=str,
+        required=True,
+        help="Check ipv4 route in routing table",
+    )
+    ArgParse.add_argument(
+        "-a", "--asn", type=str, required=True, help="ASN that we want to check"
+    )
+    ArgParse.add_argument(
+        "-s", "--sleep", type=int, required=True, help="Sleep for how long"
+    )
+    ArgParse.add_argument(
+        "-t",
+        "--transit",
+        type=str,
+        required=True,
+        help="A list of transit asns eg: '174,1299,3356,5511,3257,2914,7922,1221,4637'",
+    )
+    ARGS = ArgParse.parse_args()
+
+except Exception as e:
+    console.log(repr(e))
+    sys.exit(1)
+
+
+def parse_routeview(output):
+    TRANSIT = ARGS.transit.split(",")
+    transit_seen = []
+    peer = 0
+    transit = 0
+    for i in output.splitlines():
+        try:
+            if ARGS.asn in i and "show" not in i:
+                as_path = str(i.split(",")[0]).strip()
+                upstream_as = as_path.split()[-2]
+                if upstream_as in TRANSIT:
+                    transit_seen.append(upstream_as)
+                    transit += 1
+                else:
+                    peer += 1
+        except ValueError as e:
+            console.log(f"fail to parse {i}\n{repr(e)}")
+    if transit_seen:
+        console.log(f"transit path:{transit} unique transit:{set(transit_seen)}")
+    else:
+        console.log("transit path:0")
+    console.log(f"peering path:{peer}")
+
+
+def main():
+    console.log("connecting to routeview")
+    routeviews = pexpect.spawn("telnet route-views.routeviews.org")
+    routeviews.expect("Username:")
+    console.log("sending username")
+    routeviews.sendline("rviews")
+    routeviews.expect("route-views>")
+    console.log("connected to routeview")
+    routeviews.sendline("terminal length 0")
+    routeviews.expect("route-views>")
+    while True:
+        routeviews.sendline(f"show ip bgp {ARGS.prefix} | include {ARGS.asn},")
+        routeviews.expect("route-views>")
+        parse_routeview(str(routeviews.before.decode("ascii")))
+        time.sleep(ARGS.sleep)
+
+
+if __name__ == "__main__":
+    main()
